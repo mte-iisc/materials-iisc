3203c80830e9d9321fbd52e57c1012909ffc70c8
goths19 <goths19@gmail.com>
2020-09-15 17:48:37 +0530

    Minor edit

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,24 +2,24 @@ var store = [{
         "title": "New facilities page up",
         "excerpt":"Check out the new facilities  ","categories": [],
         "tags": [],
-        "url": "https://mte-iisc.github.io/materials-iisc/highlights/new-facilities/",
-        "teaser": "https://mte-iisc.github.io/materials-iisc/assets/images/slider/reunion2019.jpg"
+        "url": "http://localhost:4000/materials-iisc/highlights/new-facilities/",
+        "teaser": "http://localhost:4000/materials-iisc/assets/images/slider/reunion2019.jpg"
       },{
         "title": "New website up",
         "excerpt":"Revamped departmental website is now up!  ","categories": [],
         "tags": [],
-        "url": "https://mte-iisc.github.io/materials-iisc/highlights/new-website/",
-        "teaser": "https://mte-iisc.github.io/materials-iisc/assets/images/IISclogo.png"
+        "url": "http://localhost:4000/materials-iisc/highlights/new-website/",
+        "teaser": "http://localhost:4000/materials-iisc/assets/images/IISclogo.png"
       },{
         "title": "New facilities page up",
         "excerpt":"Check out the new facilities K.P. Abraham Memorial Fund was launched in 2012 with a goal of raising Rs. 2 crores to serve the twin objectives of equipping our new building (see below), and endowing a visiting faculty chair. We have already received generous contributions from our alumni and well...","categories": [],
         "tags": [],
-        "url": "https://mte-iisc.github.io/materials-iisc/news/new-facilities/",
-        "teaser": "https://mte-iisc.github.io/materials-iisc/assets/images/slider/reunion2019.jpg"
+        "url": "http://localhost:4000/materials-iisc/news/new-facilities/",
+        "teaser": "http://localhost:4000/materials-iisc/assets/images/slider/reunion2019.jpg"
       },{
         "title": "New website up",
         "excerpt":"Revamped departmental website is now up!  ","categories": [],
         "tags": [],
-        "url": "https://mte-iisc.github.io/materials-iisc/news/new-website/",
-        "teaser": "https://mte-iisc.github.io/materials-iisc/assets/images/IISclogo.png"
+        "url": "http://localhost:4000/materials-iisc/news/new-website/",
+        "teaser": "http://localhost:4000/materials-iisc/assets/images/IISclogo.png"
       },]
