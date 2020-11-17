83d8b4335c5aeda1358e3bad77826eb044798e32
goths19 <goths19@gmail.com>
2020-11-17 12:59:51 +0530

    Cleanup news page

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -41,12 +41,6 @@ var store = [{
         "url": "https://mte-iisc.github.io/materials-iisc/highlights/ebsd-ti6al4v",
         "teaser": "https://mte-iisc.github.io/materials-iisc/assets/images/research/ebsdti6al4v.jpg"
       },{
-        "title": "New facilities page up",
-        "excerpt":"Check out the new facilities K.P. Abraham Memorial Fund was launched in 2012 with a goal of raising Rs. 2 crores to serve the twin objectives of equipping our new building (see below), and endowing a visiting faculty chair. We have already received generous contributions from our alumni and well...","categories": [],
-        "tags": [],
-        "url": "https://mte-iisc.github.io/materials-iisc/news/new-facilities/",
-        "teaser": "https://mte-iisc.github.io/materials-iisc/assets/images/slider/reunion2019.jpg"
-      },{
         "title": "New website up",
         "excerpt":"Revamped departmental website is now up!  ","categories": [],
         "tags": [],
