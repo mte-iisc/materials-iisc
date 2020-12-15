a046a514209c53ef9f4b4ba0870f0c0ff365516b
goths19 <goths19@gmail.com>
2020-12-15 12:27:39 +0530

    Trying to change baseurl

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,48 +2,48 @@ var store = [{
         "title": "Machining thin metallic foils",
         "excerpt":"Machining thin metallic foils by applying electric current pulse and a mechanical stress: synergy between variuos fields. Detailed description coming soon.  ","categories": [],
         "tags": [],
-        "url": "https://mte-iisc.github.io/materials-iisc/highlights/thin-foils",
-        "teaser": "https://mte-iisc.github.io/materials-iisc/assets/images/research/machining.jpg"
+        "url": "https://mte-iisc.github.io/highlights/thin-foils",
+        "teaser": "https://mte-iisc.github.io/assets/images/research/machining.jpg"
       },{
         "title": "High throughput creep",
         "excerpt":"High throughput determination of creep response of materials, saving time and resources by more than 10 times. Detailed description coming soon.  ","categories": [],
         "tags": [],
-        "url": "https://mte-iisc.github.io/materials-iisc/highlights/high-throughput-creep",
-        "teaser": "https://mte-iisc.github.io/materials-iisc/assets/images/research/mechanicaltesting.jpg"
+        "url": "https://mte-iisc.github.io/highlights/high-throughput-creep",
+        "teaser": "https://mte-iisc.github.io/assets/images/research/mechanicaltesting.jpg"
       },{
         "title": "Visualisation of stem cells",
         "excerpt":"Visualisation of stem cells. Detailed description coming soon.  ","categories": [],
         "tags": [],
-        "url": "https://mte-iisc.github.io/materials-iisc/highlights/stem-cells",
-        "teaser": "https://mte-iisc.github.io/materials-iisc/assets/images/research/stemcells1.jpg"
+        "url": "https://mte-iisc.github.io/highlights/stem-cells",
+        "teaser": "https://mte-iisc.github.io/assets/images/research/stemcells1.jpg"
       },{
         "title": "Inverse Blech Length",
         "excerpt":"Discovery of Inverse Blech Length in presence of thermomigration and electromigration in thin films. Detailed description coming soon.  ","categories": [],
         "tags": [],
-        "url": "https://mte-iisc.github.io/materials-iisc/highlights/inverse-blech-length",
-        "teaser": "https://mte-iisc.github.io/materials-iisc/assets/images/research/thinfilm.jpg"
+        "url": "https://mte-iisc.github.io/highlights/inverse-blech-length",
+        "teaser": "https://mte-iisc.github.io/assets/images/research/thinfilm.jpg"
       },{
         "title": "Atoms resolved by APT in 100 cubic planes",
         "excerpt":"Atoms resolved by APT in 100 cubic planes. Detailed description coming soon.  ","categories": [],
         "tags": [],
-        "url": "https://mte-iisc.github.io/materials-iisc/highlights/atom-planes-apt",
-        "teaser": "https://mte-iisc.github.io/materials-iisc/assets/images/research/atoms.jpg"
+        "url": "https://mte-iisc.github.io/highlights/atom-planes-apt",
+        "teaser": "https://mte-iisc.github.io/assets/images/research/atoms.jpg"
       },{
         "title": "Microstructure of additively manufactured stainless steel",
         "excerpt":"Microstructure of additively manufactured 316L stainless steel subjected to tensile loading. Detailed description coming soon.  ","categories": [],
         "tags": [],
-        "url": "https://mte-iisc.github.io/materials-iisc/highlights/316l-ss",
-        "teaser": "https://mte-iisc.github.io/materials-iisc/assets/images/research/TensileLoading.jpg"
+        "url": "https://mte-iisc.github.io/highlights/316l-ss",
+        "teaser": "https://mte-iisc.github.io/assets/images/research/TensileLoading.jpg"
       },{
         "title": "Micrographs of additively manufactured Ti-6Al-4V",
         "excerpt":"EBSD micrographs of additively manufactured Ti-6Al-4V after heat treatment. Detailed description coming soon.  ","categories": [],
         "tags": [],
-        "url": "https://mte-iisc.github.io/materials-iisc/highlights/ebsd-ti6al4v",
-        "teaser": "https://mte-iisc.github.io/materials-iisc/assets/images/research/ebsdti6al4v.jpg"
+        "url": "https://mte-iisc.github.io/highlights/ebsd-ti6al4v",
+        "teaser": "https://mte-iisc.github.io/assets/images/research/ebsdti6al4v.jpg"
       },{
         "title": "New website up",
         "excerpt":"Revamped departmental website is now up!  ","categories": [],
         "tags": [],
-        "url": "https://mte-iisc.github.io/materials-iisc/news/new-website/",
-        "teaser": "https://mte-iisc.github.io/materials-iisc/assets/images/IISclogo.png"
+        "url": "https://mte-iisc.github.io/news/new-website/",
+        "teaser": "https://mte-iisc.github.io/assets/images/IISclogo.png"
       },]
