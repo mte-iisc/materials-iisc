94759849aaffc3ef14820e22fd5830ef951eee28
suryakaanth <rravi@iisc.ac.in>
2021-08-17 11:46:27 +0530

    Esakki award added

--- _news/2021-08-17-esakki-dimat-award.md
+++ _news/2021-08-17-esakki-dimat-award.md
@@ -1,5 +1,5 @@
 ---
-title: "Neelamegan Esakkiraja wins the Best Young Scientist of DIMAT 2021)"
-permalink: /awards/esakki-17-august-2021
+title: "Neelamegan Esakkiraja wins the Best Young Scientist of DIMAT 2021 (17/08/21)"
+permalink: /awards/esakki-17-aug-2021
 ---
 "A joint work done by Anuj Dash and Neelamegan Esakkiraja, which was presented by Esakkiraja has won the Best Young Scientist of DIMAT 2021 for the talk on Tracer diffusion coefficients in NiCoFeCr estimated by pseudo-binary, pseudo-ternary diffusion couple and radiotracer methods"
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -335,9 +335,9 @@ var store = [{
         "url": "https://materials.iisc.ac.in/colloquium/panithi-17-aug-2021",
         "teaser": null
       },{
-        "title": "Neelamegan Esakkiraja wins the Best Young Scientist of DIMAT 2021)",
+        "title": "Neelamegan Esakkiraja wins the Best Young Scientist of DIMAT 2021 (17/08/21)",
         "excerpt":"“A joint work done by Anuj Dash and Neelamegan Esakkiraja, which was presented by Esakkiraja has won the Best Young Scientist of DIMAT 2021 for the talk on Tracer diffusion coefficients in NiCoFeCr estimated by pseudo-binary, pseudo-ternary diffusion couple and radiotracer methods”  ","categories": [],
         "tags": [],
-        "url": "https://materials.iisc.ac.in/awards/esakki-17-august-2021",
+        "url": "https://materials.iisc.ac.in/awards/esakki-17-aug-2021",
         "teaser": null
       },]
