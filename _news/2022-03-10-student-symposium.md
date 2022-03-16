5a10528d4a638603751019835843841355e10056
suryakaanth <rravi@iisc.ac.in>
2022-03-16 15:27:10 +0530

    news updated

--- _news/2022-03-10-student-symposium.md
+++ _news/2022-03-10-student-symposium.md
@@ -1,7 +1,7 @@
 ---
-title: "Materials Engineering Student Symposium 2022 (02/04/22)"
+title: "Materials Engineering Student Symposium 2022 (12/04/22)"
 ---
-Materials Engineering Student Symposium 2022 is on 2nd and 3rd April 2022. This year's symposium will be an offline event. The last date of sending the abstract for the platform presentation (all day 2nd April and forenoon of 3rd April 2022) and poster session (afternoon of 3rd April) is 25th March 2022. There will be a dinner on 3rd April evening in the department. 
+Materials Engineering Student Symposium 2022 is on 12th and 13th April 2022. This year's symposium will be an offline event. The last date of sending the abstract for the platform presentation (all day 12th April and forenoon of 13th April 2022) and poster session (afternoon of 13th April) is 13th April 2022. There will be a dinner on 13th April evening in the department. 
 
 Please email your abstracts to:
 
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -563,8 +563,8 @@ var store = [{
         "url": "https://materials.iisc.ac.in/seminar/syed-10-mar-2022",
         "teaser": null
       },{
-        "title": "Materials Engineering Student Symposium 2022 (02/04/22)",
-        "excerpt":"Materials Engineering Student Symposium 2022 is on 2nd and 3rd April 2022. This year’s symposium will be an offline event. The last date of sending the abstract for the platform presentation (all day 2nd April and forenoon of 3rd April 2022) and poster session (afternoon of 3rd April) is 25th March 2022. There will be a dinner on 3rd April evening in the department.   Please email your abstracts to:   A) Akhand Pratap Singh: akhandsingh@iisc.ac.in  B) Mohan Kumar Guttula mohankg@iisc.ac.in  C) Swapnil Bhure swapnilbhure@iisc.ac.in  ","categories": [],
+        "title": "Materials Engineering Student Symposium 2022 (12/04/22)",
+        "excerpt":"Materials Engineering Student Symposium 2022 is on 12th and 13th April 2022. This year’s symposium will be an offline event. The last date of sending the abstract for the platform presentation (all day 12th April and forenoon of 13th April 2022) and poster session (afternoon of 13th April) is 13th April 2022. There will be a dinner on 13th April evening in the department.   Please email your abstracts to:   A) Akhand Pratap Singh: akhandsingh@iisc.ac.in  B) Mohan Kumar Guttula mohankg@iisc.ac.in  C) Swapnil Bhure swapnilbhure@iisc.ac.in  ","categories": [],
         "tags": [],
         "url": "https://materials.iisc.ac.in/news/2022-03-10-student-symposium/",
         "teaser": null
