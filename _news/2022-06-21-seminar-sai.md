1d08f025963baac13bc7ba64defc2a518c392793
suryakaanth <rravi@iisc.ac.in>
2022-06-21 09:36:27 +0530

    news updated

---
+++ _news/2022-06-21-seminar-sai.md
@@ -0,0 +1,17 @@
+---
+title: "Green synthesis of a novel porous gold-curcumin nanocomposite for super-efficient alcohol oxidation (08/06/22)"
+permalink: /seminar/sai-21-jun-2022
+---
+
+### Speaker and Affliation:
+Sai Prasad Nayak<br>
+Sri Sathya Sai Institute of Higher Learning
+
+### When?
+21<sup>st</sup> June, 2022 (Tuesday), 04:00 PM (India Standard Time)
+
+### Location
+Department of Materials Engineering, KPA auditorium
+
+### Abstract
+Engineering nanomaterials for non-invasive electrochemical detection of dopamine (DA) in biological samples has been daunting. We report a novel gold-curcumin (Au-CM) nanocomposite as an electrochemical sensor for real-time ultra-selective detection of DA in urine samples. Gold nanoparticles (~2-3 nm) encased in porous curcumin (CM) network on the surface of a glassy carbon electrode were synthesized via a galvanostatic method and used as the electrochemical sensor. The modified electrode exhibited excellent sensitivity and selectivity toward DA sensing with a record-low limit of detection (LOD) of 3 pM. Furthermore, a wide range of detection 1 pM – 400 M (R2=0.99) was achieved at pH 6. DA detection was successfully performed in pharmaceutical formulations and urine samples without spiking and any pre-treatment procedures, overcoming the complexity of biofluids with results comparable to the clinical values.
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -707,6 +707,12 @@ var store = [{
         "url": "https://materials.iisc.ac.in/defence/prantosh-16-jun-2022",
         "teaser": null
       },{
+        "title": "Green synthesis of a novel porous gold-curcumin nanocomposite for super-efficient alcohol oxidation (08/06/22)",
+        "excerpt":"Speaker and Affliation:  Sai Prasad Nayak  Sri Sathya Sai Institute of Higher Learning   When?  21st June, 2022 (Tuesday), 04:00 PM (India Standard Time)   Location  Department of Materials Engineering, KPA auditorium   Abstract  Engineering nanomaterials for non-invasive electrochemical detection of dopamine (DA) in biological samples has been daunting. We report a novel gold-curcumin (Au-CM) nanocomposite as an electrochemical sensor for real-time ultra-selective detection of DA in urine samples. Gold nanoparticles (~2-3 nm) encased in porous curcumin (CM) network on the surface of a glassy carbon electrode were synthesized via a galvanostatic method and used as the electrochemical sensor. The modified electrode exhibited excellent sensitivity and selectivity toward DA sensing with a record-low limit of detection (LOD) of 3 pM. Furthermore, a wide range of detection 1 pM – 400 M (R2=0.99) was achieved at pH 6. DA detection was successfully performed in pharmaceutical formulations and urine samples without spiking and any pre-treatment procedures, overcoming the complexity of biofluids with results comparable to the clinical values.  ","categories": [],
+        "tags": [],
+        "url": "https://materials.iisc.ac.in/seminar/sai-21-jun-2022",
+        "teaser": null
+      },{
         "title": "PhD Thesis Colloquium: Ms. Sujata Singh (28/06/22)",
         "excerpt":"Thesis title:  Enhancement in the Corrosion Resistance of Selected High Entropy  Alloys by Incorporation of Carbon Nanotubes   Faculty advisor(s):  Prof. Chandan Srivastava   When?  28th June, 2022 (Tuesday), 10:30 AM (India Standard Time)   Location  Microsoft Teams Meet-up  ","categories": [],
         "tags": [],
