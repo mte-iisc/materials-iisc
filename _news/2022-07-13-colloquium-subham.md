4da1957a4bf66ea7a088694a8337dfde796b4729
suryakaanth <rravi@iisc.ac.in>
2022-07-12 14:56:57 +0530

    news updated

---
+++ _news/2022-07-13-colloquium-subham.md
@@ -0,0 +1,15 @@
+---
+title: "PhD Thesis Colloquium: Mr. Subham Mridha (13/07/22)"
+permalink: /colloquium/subham-28-jul-2022
+---
+### Thesis title:
+MULTI-SCALE MODELLING OF VACANCY–GRAIN BOUNDARY INTERACTIONS
+
+### Faculty advisor(s):
+Prof. S. Karthikeyan 
+
+### When?
+13<sup>th</sup> July, 2022 (Wednesday), 3:30 PM (India Standard Time)
+
+### Location
+K.P. Abraham Auditorium
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -730,4 +730,10 @@ var store = [{
         "tags": [],
         "url": "https://materials.iisc.ac.in/seminar/apparao-06-jul-2022",
         "teaser": null
+      },{
+        "title": "PhD Thesis Colloquium: Mr. Subham Mridha (13/07/22)",
+        "excerpt":"Thesis title:  MULTI-SCALE MODELLING OF VACANCY–GRAIN BOUNDARY INTERACTIONS   Faculty advisor(s):  Prof. S. Karthikeyan   When?  13th July, 2022 (Wednesday), 3:30 PM (India Standard Time)   Location  K.P. Abraham Auditorium  ","categories": [],
+        "tags": [],
+        "url": "https://materials.iisc.ac.in/colloquium/subham-28-jul-2022",
+        "teaser": null
       },]
