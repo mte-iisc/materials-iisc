ea3945fa8120435dd583f209d20a876a2a93d776
suryakaanth <rravi@iisc.ac.in>
2022-09-16 09:37:12 +0530

    news updated

---
+++ _news/2021-09-22-defence-priya.md
@@ -0,0 +1,15 @@
+---
+title: "PhD Thesis Defence: Ms. Priya Goel (21/09/22)"
+permalink: /defence/priya-21-sep-2022
+---
+### Thesis title:
+Extraction of equivalent uniaxial plastic and viscoplastic behavior from bending using a mechanistic approach
+
+### Faculty advisor(s):
+Prof. Vikram Jayaram 
+
+### When?
+21<sup>st</sup> September, 2022 (Wednesday), 12:00 PM (India Standard Time)
+
+### Location
+KPA Auditorium, Department of Materials Engineering
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -743,6 +743,12 @@ var store = [{
         "url": "https://materials.iisc.ac.in/highlights/microsim-lab",
         "teaser": "https://materials.iisc.ac.in/assets/images/slider/microsim_lab.png"
       },{
+        "title": "PhD Thesis Defence: Ms. Priya Goel (21/09/22)",
+        "excerpt":"Thesis title:  Extraction of equivalent uniaxial plastic and viscoplastic behavior from bending using a mechanistic approach   Faculty advisor(s):  Prof. Vikram Jayaram   When?  21st September, 2022 (Wednesday), 12:00 PM (India Standard Time)   Location  KPA Auditorium, Department of Materials Engineering  ","categories": [],
+        "tags": [],
+        "url": "https://materials.iisc.ac.in/defence/priya-21-sep-2022",
+        "teaser": null
+      },{
         "title": "PhD Thesis Colloquium: Ms. Sujata Singh (17/08/22)",
         "excerpt":"Thesis title:  Enhancement in the Corrosion Resistance of Selected High Entropy Alloys by Incorporation of Carbon Nanotubes   Faculty advisor(s):  Prof. Chandan Srivastava   When?  17th August, 2022 (Wednesday), 3:00 PM (India Standard Time)   Mode  Online via Microsoft Teams https://teams.microsoft.com/l/meetup-join/19%3ameeting_MmI0YmVhMTEtYjUxMy00NWZkLWIzYTctNmFkYTQwOTAyZTMy%40thread.v2/0?context=%7b%22Tid%22%3a%226f15cd97-f6a7-41e3-b2c5-ad4193976476%22%2c%22Oid%22%3a%220adae817-f942-4c15-8bde-73332579a6ee%22%7d  ","categories": [],
         "tags": [],
