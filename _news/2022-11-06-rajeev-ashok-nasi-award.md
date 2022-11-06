171a56acb1dcf4df48249f7b9c423370574cc4e0
suryakaanth <rravi@iisc.ac.in>
2022-11-06 09:26:39 +0530

    news updated

---
+++ _news/2022-11-06-rajeev-ashok-nasi-award.md
@@ -0,0 +1,7 @@
+---
+title: "Profs. Rajeev Ranjan and Ashok Raichur have been elected as fellows of National Academy of Sciences India (NASI) (06/11/22)"
+permalink: /awards/rajeev-06-nov-2022
+---
+Congratulations !!!
+
+Profs. Rajeev Ranjan and Ashok Raichur have been elected as fellows of National Academy of Sciences India (NASI).
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -803,6 +803,12 @@ var store = [{
         "url": "https://materials.iisc.ac.in/seminar/jayasree-02-nov-2022",
         "teaser": null
       },{
+        "title": "Profs. Rajeev Ranjan and Ashok Raichur have been elected as fellows of National Academy of Sciences India (NASI) (06/11/22)",
+        "excerpt":"Congratulations !!!   Profs. Rajeev Ranjan and Ashok Raichur have been elected as fellows of National Academy of Sciences India (NASI).  ","categories": [],
+        "tags": [],
+        "url": "https://materials.iisc.ac.in/awards/rajeev-06-nov-2022",
+        "teaser": null
+      },{
         "title": "PhD Thesis Colloquium: Ms. Smita Kamble (07/11/22)",
         "excerpt":"Thesis title:  Study of gas-fine flow in a packed bed with an application to the Ironmaking Blast Furnace   Faculty advisor(s):  Prof. Govind S. Gupta   When?  7th November, 2022 (Monday), 3:30 PM (India Standard Time)   Mode  Online via Microsoft Teams https://teams.microsoft.com/l/meetup-join/19%3ameeting_MjlkN2JlMTktNWQzMi00YTFjLWJlYmUtZmRmMWUxMzFhYzZi%40thread.v2/0?context=%7b%22Tid%22%3a%226f15cd97-f6a7-41e3-b2c5-ad4193976476%22%2c%22Oid%22%3a%220adae817-f942-4c15-8bde-73332579a6ee%22%7d  ","categories": [],
         "tags": [],
