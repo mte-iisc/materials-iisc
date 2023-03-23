701d9a238427efd0bee51cc06652fd84a9ff850b
suryakaanth <rravi@iisc.ac.in>
2023-03-23 10:07:32 +0530

    news updated

---
+++ _news/2023-03-23-seminar-disha.md
@@ -0,0 +1,18 @@
+---
+title: "Demystifying the Ph.D. Application Process: Insights and Advice (23/03/23)"
+permalink: /seminar/disha-23-mar-2023
+---
+### Speaker & Affiliation:
+Disha Bandyopadhyay<br>
+Researcher (Materials)<br>
+ 
+### When ?
+23rd March, 2023 (Thursday), 4.00 pm (IST)
+
+### Venue
+KPA Auditorium, Department of Materials Engineering (Online through Microsoft Teams)
+
+### Short Introduction: 
+Disha is a materials scientist with an academic background in materials science and nuclear engineering. She applied and was accepted into Ph.D. programs in the UK, US, and Switzerland before ultimately deciding to pursue her PhD at EPFL, where she will focus on research related to light-matter interactions and additive manufacturing. Disha's research interests are driven by the desire to positively impact society, and she is committed to finding solutions to real-world problems. Outside of her research, Disha enjoys reading and crocheting. She is eager to use her expertise to make meaningful contributions to the scientific community.
+
+Organizing Team (Students): Aryan Kumar, Anney Kumar Sinha, Ashutosh Kumar, Maadhav, D.V. Praveen Kumar, Mukesh Jain, Ankit Kumar Gupta, Gouranga Behera, Souptik Dey, Dhiraj S. Kori
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1006,4 +1006,10 @@ var store = [{
         "tags": [],
         "url": "https://materials.iisc.ac.in/seminar/nicolaie-21-mar-2023",
         "teaser": null
+      },{
+        "title": "Demystifying the Ph.D. Application Process: Insights and Advice (23/03/23)",
+        "excerpt":"Speaker &amp; Affiliation:  Disha Bandyopadhyay  Researcher (Materials)    When ?  23rd March, 2023 (Thursday), 4.00 pm (IST)   Venue  KPA Auditorium, Department of Materials Engineering (Online through Microsoft Teams)   Short Introduction:  Disha is a materials scientist with an academic background in materials science and nuclear engineering. She applied and was accepted into Ph.D. programs in the UK, US, and Switzerland before ultimately deciding to pursue her PhD at EPFL, where she will focus on research related to light-matter interactions and additive manufacturing. Disha’s research interests are driven by the desire to positively impact society, and she is committed to finding solutions to real-world problems. Outside of her research, Disha enjoys reading and crocheting. She is eager to use her expertise to make meaningful contributions to the scientific community.   Organizing Team (Students): Aryan Kumar, Anney Kumar Sinha, Ashutosh Kumar, Maadhav, D.V. Praveen Kumar, Mukesh Jain, Ankit Kumar Gupta, Gouranga Behera, Souptik Dey, Dhiraj S. Kori  ","categories": [],
+        "tags": [],
+        "url": "https://materials.iisc.ac.in/seminar/disha-23-mar-2023",
+        "teaser": null
       },]
