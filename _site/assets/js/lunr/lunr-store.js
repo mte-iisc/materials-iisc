e23bbde1eefc5e0589673bc94515926d8be0e3fe
suryakaanth <rravi@iisc.ac.in>
2023-03-24 14:31:30 +0530

    news updated

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1024,4 +1024,10 @@ var store = [{
         "tags": [],
         "url": "https://materials.iisc.ac.in/colloquium/muni-27-mar-2023",
         "teaser": null
+      },{
+        "title": "PhD Thesis Colloquium: Mr. Deepak Kumar (30/03/23)",
+        "excerpt":"Thesis title:  Microstructure-texture-mechanical property correlation in additively manufactured stainless Steel 316L and Cu-Ni-Sn   Faculty advisor(s):  Prof. Satyam Suwas   When?  30th March, 2023 (Thursday), 04:00 PM (India Standard Time)   Where  KPA Auditorium, Department of Materials Engineering   Abstract  Metal additive manufacturing (AM) has gained considerable interest because of its ability of near net shape building of the components, one-go production, ability to make intricate designs and minimized wastage. Further, the mechanical properties of the manufactured alloys are sometimes reported to be superior due to various microstructural attributes.   This presentation deals with the work carried out to examine the evolution of microstructure, crystallographic texture and their influence on the mechanical properties on the material system 316L manufactured through selective laser melting (SLM) and wire arc additive manufacturing (WAAM). The effect of different scan strategies such as inter-layer and intra-layer on microstructure-texture-mechanical property correlation has been investigated on SLM 316L. The benefits of as built microstructure and texture of SLM and WAAM 316L on mechanical properties has been critically evaluated through tension and fracture toughness studies. Moreover, the role of important heat treatment on thermal stability of structure and mechanical properties are also examined. Apart from this, a spinodal hardened alloy system Cu-15Ni-8Sn manufactured through SLM has also been studied in order to find how benefits of additive manufacturing behaves in this alloy. The role of ageing treatment that brings spinodal hardening in Cu-15Ni-8Sn is also investigated in the present study.  ","categories": [],
+        "tags": [],
+        "url": "https://materials.iisc.ac.in/colloquium/deepak-30-mar-2023",
+        "teaser": null
       },]
