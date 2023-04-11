7f572f1ade3e249e78f086354f4d78d5c3891134
suryakaanth <rravi@iisc.ac.in>
2023-04-11 10:27:11 +0530

    news updated

---
+++ _news/2023-04-11-seminar-hariprasad.md
@@ -0,0 +1,16 @@
+---
+title: "Deconvoluting residual stress effects on the toughness of (Hf-Nb-Ta-Zr)C multi-element carbide coatings (11/04/23)"
+permalink: /seminar/hariprasad-11-apr-2023
+---
+### Speaker & Affiliation:
+Dr. Hariprasad Gopalan <br>
+Institute of Applied Materials, Karslruhe Institute of Technology, Karlsruhe<br>
+ 
+### When ?
+11th April, 2023 (Tuesday), 4.00 pm (IST)
+
+### Venue
+KPA Auditorium, Department of Materials Engineering
+
+### Abstract: 
+Multi-element hard coatings inspired by high entropy alloys offer a way to explore the hardness-toughness tradeoff in materials science. In this talk, I will present sputter deposited (Hf-Nb-Ta-Zr)C as a promising material to obtain super hardness and respectable toughness. Extensive microstructural and mechanical characterization was performed to understand the specific roles of microstructure and residual stress on the fracture behaviour. A combination of nanoindentation and in situ microcantilever toughness testing quantitatively reveals the relationship between apparent and true toughness of hard coatings. 
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1042,4 +1042,10 @@ var store = [{
         "tags": [],
         "url": "https://materials.iisc.ac.in/seminar/chandan-10-apr-2023",
         "teaser": null
+      },{
+        "title": "Deconvoluting residual stress effects on the toughness of (Hf-Nb-Ta-Zr)C multi-element carbide coatings (11/04/23)",
+        "excerpt":"Speaker &amp; Affiliation:  Dr. Hariprasad Gopalan   Institute of Applied Materials, Karslruhe Institute of Technology, Karlsruhe    When ?  11th April, 2023 (Tuesday), 4.00 pm (IST)   Venue  KPA Auditorium, Department of Materials Engineering   Abstract:  Multi-element hard coatings inspired by high entropy alloys offer a way to explore the hardness-toughness tradeoff in materials science. In this talk, I will present sputter deposited (Hf-Nb-Ta-Zr)C as a promising material to obtain super hardness and respectable toughness. Extensive microstructural and mechanical characterization was performed to understand the specific roles of microstructure and residual stress on the fracture behaviour. A combination of nanoindentation and in situ microcantilever toughness testing quantitatively reveals the relationship between apparent and true toughness of hard coatings.  ","categories": [],
+        "tags": [],
+        "url": "https://materials.iisc.ac.in/seminar/hariprasad-11-apr-2023",
+        "teaser": null
       },]
