48559f87ee6edc1a78e8c6872a0f342b0e2384e8
suryakaanth <rravi@iisc.ac.in>
2024-07-15 21:32:40 +0530

    news updated

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1588,4 +1588,10 @@ var store = [{
         "tags": [],
         "url": "https://materials.iisc.ac.in/colloquium/sruthi-16-jul-2024",
         "teaser": null
+      },{
+        "title": "Direct Electron EBSD detector (18/07/24)",
+        "excerpt":"Speaker and Affliation:  Dr. Jakub Holzer  ThermoFisher Scientific in Brno, Czech Republic   When?  18th July, 2024 (Thursday), 03.30 PM (India Standard Time)   Where  KPA Auditorium, Dept. of Materials Engineering, IISc, Bangalore   Abstract:  Direct electron detection is an emerging technology in the field of detection science and the industry is slowly undergoing the same change and it experienced when Si-Li EDS detector became obsolete with arrival of silicon drift detectors. The talk will focus on advantages of direct electron detection sensors used in SEM and SDBs, especially in Thermo Fisher Scientific, Inc. portfolio of microscopes. The main topics will be standard EBSD geometry (70 deg tilt), Reflection Kikuchi Diffraction and 4DSTEM/TKD application   Speaker Bio:  Jakub Holzer, Ph.D. works as an application scientist at ThermoFisher Scientific in Brno, Czech republic. He is responsible for the application development of mid-range dual beams and diffraction based analytical and imaging techniques. He acquired his Ph.D. from Central European Institute of Technology while working at Institute of Physics of Materials of Czech Academy of Sciences.  ","categories": [],
+        "tags": [],
+        "url": "https://materials.iisc.ac.in/seminar/jakub-18-jul-2024",
+        "teaser": null
       },]
